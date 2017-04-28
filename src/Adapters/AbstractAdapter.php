9186d62d2d0931e746888628ffa0ebfc9bfbf30d
Damiano Petrungaro <damianopetrungaro@gmail.com>
2017-04-28 18:06:26 +0200

    Wrapped DateTime reflection(Closure) in try catch

--- src/Adapters/AbstractAdapter.php
+++ src/Adapters/AbstractAdapter.php
@@ -5,6 +5,7 @@ namespace Moon\Cache\Adapters;
 use Closure;
 use Moon\Cache\Collection\CacheItemCollection;
 use Moon\Cache\Collection\CacheItemCollectionInterface;
+use Moon\Cache\Exception\CacheInvalidArgumentException;
 use Psr\Cache\CacheItemInterface;
 
 abstract class AbstractAdapter implements AdapterInterface
@@ -38,9 +39,21 @@ abstract class AbstractAdapter implements AdapterInterface
      */
     protected function retrieveExpiringDateFromCacheItem(CacheItemInterface $cacheItem): \DateTimeImmutable
     {
-        return Closure::bind(function (CacheItemInterface $cacheItem, $expirationParameterName) {
-            return $cacheItem->$expirationParameterName;
-        }, null, $cacheItem)->__invoke($cacheItem, $this->expirationParameterName);
+        try {
+            $expirationDate = Closure::bind(function (CacheItemInterface $cacheItem, $expirationParameterName) {
+                return $cacheItem->$expirationParameterName;
+            }, null, $cacheItem)->__invoke($cacheItem, $this->expirationParameterName);
+
+            if ($expirationDate instanceof \DateTime) {
+                $expirationDate = \DateTimeImmutable::createFromMutable($expirationDate);
+            }
+
+            return $expirationDate;
+        } catch (\Exception $e) {
+            throw new CacheInvalidArgumentException(
+                "The CacheItemInterface object haven't any {$this->expirationParameterName} attribute.", 0, $e
+            );
+        }
     }
 
     /**
